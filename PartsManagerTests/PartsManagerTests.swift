d1f1037769fce8ec85feb1d49a9899703a85a775
MeglSiki <megl000.dev@gmail.com>
2020-08-12 17:56:06 +0900

    Initial Commit

---
+++ PartsManagerTests/PartsManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  PartsManagerTests.swift
+//  PartsManagerTests
+//
+//  Created by 坂井貴樹 on 2020/08/12.
+//  Copyright © 2020 MeglSiki. All rights reserved.
+//
+
+import XCTest
+@testable import PartsManager
+
+class PartsManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
